3bd861f54f0fab221d3240fd42a2588fc85a4e52
Dragon <aleha2552654@gmail.com>
2026-01-19 14:44:35 +0300

    feat(MiniShop.AuthService.Domain) add entity User

---
+++ src/MiniShop.AuthService.Domain/Entities/User.cs
@@ -0,0 +1,41 @@
+﻿namespace MiniShop.AuthService.Domain.Entities
+{
+    public class User
+    {
+        public Guid Id { get; private set; }
+
+        public string Email { get; private set; }
+
+        public string PasswordHash { get; private set; }
+
+        public List<string> Roles { get; private set; } = new();
+
+        public DateTime CreatedAt { get; private set; }
+
+        public User(string email, string passwordHash)
+        {
+            Id = Guid.NewGuid();
+            Email = email;
+            PasswordHash = passwordHash;
+            CreatedAt = DateTime.UtcNow;
+        }
+
+        public void ChangePassword(string newPasswordHash)
+        {
+            PasswordHash = newPasswordHash;
+        }
+
+        public void AddRole(string role)
+        {
+            if (!Roles.Contains(role))
+            {
+                Roles.Add(role);
+            }
+        }
+
+        public void RemoveRole(string role)
+        {
+            Roles.Remove(role);
+        }
+    }
+}
