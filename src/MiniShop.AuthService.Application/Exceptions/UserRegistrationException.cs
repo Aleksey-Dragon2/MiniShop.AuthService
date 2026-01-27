be1b36f979e2b132d67d9e330c286a756a9a7a49
Dragon <aleha2552654@gmail.com>
2026-01-28 00:51:43 +0300

    feat(Application): add UserRegistrationException

---
+++ src/MiniShop.AuthService.Application/Exceptions/UserRegistrationException.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MiniShop.AuthService.Application.Exceptions
+{
+    public class UserRegistrationException : Exception
+    {
+        public IReadOnlyCollection<IdentityError> Errors { get; }
+
+        public UserRegistrationException(IEnumerable<IdentityError> errors)
+            : base("User registration failed")
+        {
+            Errors = errors.ToList().AsReadOnly();
+        }
+    }
+}
