1f35ed6f6b904d808ab3e58e63a6f3994e42f8ca
Dragon <aleha2552654@gmail.com>
2026-01-28 00:52:16 +0300

    feat(application): add RegisterUserCommand

---
+++ src/MiniShop.AuthService.Application/Users/Register/RegisterUserCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+using MiniShop.AuthService.Application.Users.Results;
+
+namespace MiniShop.AuthService.Application.Users.Register
+{
+    public record RegisterUserCommand (string UserName, string Email, string Password)
+        :IRequest<RegisterUserResult>;
+}
