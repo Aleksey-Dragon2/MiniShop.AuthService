53edeee0292e8309da82bd691dbf3a08b00a2305
Dragon <aleha2552654@gmail.com>
2026-01-28 00:52:31 +0300

    feat(application): add RegisterUserValidation

---
+++ src/MiniShop.AuthService.Application/Users/Register/RegisterUserCommandValidation.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+namespace MiniShop.AuthService.Application.Users.Register
+{
+    public class RegisterUserCommandValidation : AbstractValidator<RegisterUserCommand>
+    {
+        public RegisterUserCommandValidation()
+        {
+            RuleFor(x => x.UserName)
+                .MinimumLength(5)
+                .MaximumLength(20)
+                .NotEmpty();
+            RuleFor(x => x.Email)
+                .EmailAddress()
+                .NotEmpty();
+            RuleFor(x => x.Password)
+                .MinimumLength(6)
+                .MaximumLength(20)
+                .NotEmpty();
+        }
+    }
+}
