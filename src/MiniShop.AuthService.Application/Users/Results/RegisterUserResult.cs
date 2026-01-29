369f1006aaab183bb2ce1265fb61c08be1c8e634
Dragon <aleha2552654@gmail.com>
2026-01-29 23:39:35 +0300

    fix(Application): change type Id (int -> Guid)

--- src/MiniShop.AuthService.Application/Users/Results/RegisterUserResult.cs
+++ src/MiniShop.AuthService.Application/Users/Results/RegisterUserResult.cs
@@ -1,4 +1,4 @@
 ﻿namespace MiniShop.AuthService.Application.Users.Results
 {
-    public record RegisterUserResult(int Id, string UserName, string Email);
+    public record RegisterUserResult(Guid Id, string UserName, string Email);
 }
