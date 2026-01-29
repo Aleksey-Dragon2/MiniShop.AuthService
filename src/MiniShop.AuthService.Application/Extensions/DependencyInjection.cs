ae406e35d7cc8ef4051cbe432419e175003a8a58
Dragon <aleha2552654@gmail.com>
2026-01-29 23:43:20 +0300

    feat(application): add DI services

---
+++ src/MiniShop.AuthService.Application/Extensions/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using FluentValidation;
+using System.Runtime.CompilerServices;
+
+namespace MiniShop.AuthService.Application.Extensions
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddAplication(this IServiceCollection services)
+        {
+            services.AddMediatR(cfg =>
+            cfg.RegisterServicesFromAssemblies(typeof(DependencyInjection).Assembly));
+            services.AddAutoMapper(cfg =>
+            cfg.AddMaps(typeof(DependencyInjection).Assembly));
+
+            return services;
+        }
+
+    }
+}
