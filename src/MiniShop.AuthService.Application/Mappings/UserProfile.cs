12f9d964bf0e03988df8989398e37f5d915c190c
Dragon <aleha2552654@gmail.com>
2026-01-29 23:42:54 +0300

    feat(Application): add Mapping configuration

---
+++ src/MiniShop.AuthService.Application/Mappings/UserProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using MiniShop.AuthService.Application.Users.Results;
+using MiniShop.AuthService.Domain.Entities;
+
+namespace MiniShop.AuthService.Application.Mappings
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<User, RegisterUserResult>().ReverseMap();
+        }
+    }
+}
