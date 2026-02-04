cbc8be502413c900abe43049ec76ca9e5117e182
Dragon <aleha2552654@gmail.com>
2026-02-04 15:33:52 +0300

    feat(auth): add role service

---
+++ src/MiniShop.AuthService.Application/Abstractions/Services/IRoleService.cs
@@ -0,0 +1,9 @@
+﻿using MiniShop.AuthService.Domain.Enums;
+using MiniShop.AuthService.Domain.Entities;
+namespace MiniShop.AuthService.Application.Abstractions.Services
+{
+    public interface IRoleService
+    {
+        Task AssignRoleToUserAsync(User user, UserRole role);
+    }
+}
