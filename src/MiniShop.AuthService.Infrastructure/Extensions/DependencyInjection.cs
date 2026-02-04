86b1208fc882c6a88e33db14a3ee20e0210214bc
Dragon <aleha2552654@gmail.com>
2026-02-04 14:23:47 +0300

    feat(deps): register infrastructure services for auth

---
+++ src/MiniShop.AuthService.Infrastructure/Extensions/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MiniShop.AuthService.Application.Abstractions.Services;
+using MiniShop.AuthService.Application.Abstractions.TokenGenerator;
+using MiniShop.AuthService.Infrastructure.Implementations.TokenGenerator;
+using MiniShop.AuthService.Infrastructure.Services;
+namespace MiniShop.AuthService.Infrastructure.Extensions
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        {
+            services.AddScoped<IRoleService, RoleService>();
+            services.AddScoped<ITokenGenerator, TokenGenerator>();
+            services.AddScoped<ITokenService, TokenService>();
+            return services;
+        }
+    }
+}
