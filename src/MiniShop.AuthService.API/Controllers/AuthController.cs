ade90fbfdefc0123f2896ee6320239fcc61472d3
Dragon <aleha2552654@gmail.com>
2026-02-04 15:31:59 +0300

    feat(auth): add login endpoint

--- src/MiniShop.AuthService.API/Controllers/AuthController.cs
+++ src/MiniShop.AuthService.API/Controllers/AuthController.cs
@@ -1,9 +1,8 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using MiniShop.AuthService.Application.Users.Results;
-using Microsoft.AspNetCore.Identity.Data;
 using MiniShop.AuthService.Application.Users.Register;
-using System.Diagnostics;
+using MiniShop.AuthService.Application.Users.Login;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,11 +10,11 @@ namespace MiniShop.AuthService.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class AuthConroller : ControllerBase
+    public class AuthController : ControllerBase
     {
         private readonly IMediator _mediator;
 
-        public AuthConroller(IMediator mediator)
+        public AuthController(IMediator mediator)
         {
             _mediator = mediator;
         }
@@ -27,6 +26,14 @@ namespace MiniShop.AuthService.API.Controllers
                 request.UserName, request.Email, request.Password));
 
             return Ok(new RegisterUserResult(user.Id, user.UserName, user.Email));
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<string>> Login([FromBody] LoginUserCommand request)
+        {
+            var token =  await _mediator.Send(new LoginUserCommand(
+                request.Email, request.Password));
+            return Ok(token);
         }
     }
 }
