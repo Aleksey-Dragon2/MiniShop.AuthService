cc1070f34643f7f0c58da7274252d21b1c74fd25
Dragon <aleha2552654@gmail.com>
2026-02-04 15:34:26 +0300

    feat(auth): add jwt-token options

---
+++ src/MiniShop.AuthService.API/Extensions/DependencyInjection.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.Text;
+
+namespace MiniShop.AuthService.API.Extensions
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPresentation(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = configuration["AuthOptions:Issuer"],
+                        ValidAudience = configuration["AuthOptions:Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["AuthOptions:SecretKey"]!)),
+                        RoleClaimType = ClaimTypes.Role
+                    };
+                });
+
+            return services;
+        }
+    }
+}
