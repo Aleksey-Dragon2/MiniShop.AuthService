157bf71569b6aeaa8b0c17b212fafab7d121056c
Dragon <aleha2552654@gmail.com>
2026-02-04 14:18:09 +0300

    feat(Infrastructure): add role seeder

---
+++ src/MiniShop.AuthService.Infrastructure/Seed/RoleSedeer.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using MiniShop.AuthService.Domain.Enums;
+
+namespace MiniShop.AuthService.Infrastructure.Seed
+{
+    public static class RoleSedeer
+    {
+        public static async Task SeedAsync(RoleManager<IdentityRole<Guid>> roleManager)
+        {
+            foreach (var roleName in Enum.GetNames(typeof(UserRole)))
+            {
+                if (!await roleManager.RoleExistsAsync(roleName))
+                {
+                    var role = new IdentityRole<Guid>(roleName);
+                    await roleManager.CreateAsync(role);
+                }
+            }
+        }
+    }
+}
