f51fd40995ed672706ac6b34615489b74c9e125d
Dragon <aleha2552654@gmail.com>
2026-02-04 14:12:53 +0300

    feat(Infrastructure): added RoleService

---
+++ src/MiniShop.AuthService.Infrastructure/Services/RoleService.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using MiniShop.AuthService.Application.Abstractions.Services;
+using MiniShop.AuthService.Domain.Entities;
+using MiniShop.AuthService.Domain.Enums;
+namespace MiniShop.AuthService.Infrastructure.Services
+{
+    public class RoleService : IRoleService
+    {
+        private readonly UserManager<User> _userManager;
+        public RoleService(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+        public async Task AssignRoleToUserAsync(User user, UserRole role)
+        {
+            var roleName = role.ToString();
+            if (!await _userManager.IsInRoleAsync(user, roleName))
+                await _userManager.AddToRoleAsync(user, roleName);
+        }
+    }
+}
